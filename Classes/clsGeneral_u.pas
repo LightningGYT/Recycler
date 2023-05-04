c4afaf9d2f30d67c35b766890f15a00a74d56c97
Thomas laptop <thomas.vansteen2004@gmail.com>
2023-05-04 22:06:30 +0200

    Started working on Salt algorithm

--- Classes/clsGeneral_u.pas
+++ Classes/clsGeneral_u.pas
@@ -9,6 +9,7 @@ type
   THasher = class
   public
     function Hash(sString, sSalt: String): String;
+    function SaltGenerator: String;
   end;
 
 implementation
@@ -25,6 +26,50 @@ begin
   sHashedString := THashSHA2.GetHashString(sBuiltString);
 
   Result := sHashedString;
+end;
+
+function THasher.SaltGenerator: String;
+const
+  defualt_values = 'abcdefghijklmnopqrstuvwxyz0123456789';
+var
+  values: String;
+  iTimes, iOperation: integer;
+  I: integer;
+  J: integer;
+begin
+  Randomize;
+
+  values := defualt_values;
+
+  // Randomize the amount of times this operation is done
+  iTimes := Random(100) + 10;
+
+  for I := 1 to iTimes do
+  begin
+
+    for J := 1 to Length(values) do
+    begin
+      iOperation := Random(4);
+
+      case iOperation of
+        0:
+          begin
+          // swap with someone
+          end;
+        2:
+        begin
+          //replicate
+        end;
+        3:
+        begin
+          // disapear
+        end;
+
+      end;
+    end;
+
+  end;
+
 end;
 
 end.
