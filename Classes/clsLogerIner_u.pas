fb87c4c51b4b313a157c28838e2084f0bb66e962
Thomas laptop <thomas.vansteen2004@gmail.com>
2023-05-04 21:05:53 +0200

    Created find User function

--- Classes/clsLogerIner_u.pas
+++ Classes/clsLogerIner_u.pas
@@ -2,17 +2,24 @@ unit clsLogerIner_u;
 
 interface
 
-
+uses clsGeneral_u, dmRecycle_u,
+      SysUtils;
 
 type
   TLoginer = class
-    private
-      fLoginID : String;
-      fInUsername:String;
-      fInPassword:String;
-      function Hasher(sPassword, sSalt:String):String;
-    public
-      constructor Create(sUsername, sPassword:String);
+  private
+    fDetails: record
+      fUsername: String;
+      fLoginID: String;
+      fPassword: String;
+      fSalt: String;
+    end;
+
+    fInUsername: String;
+    fInPassword: String;
+    function FindUser: boolean;
+  public
+    constructor Create(sUsername, sPassword: String);
   end;
 
 implementation
@@ -23,7 +30,33 @@ constructor TLoginer.Create(sUsername, sPassword: String);
 begin
   fInUsername := sUsername;
   fInPassword := sPassword;
+  FindUser;
 end;
 
+function TLoginer.FindUser: boolean;
+begin
+  with dmRecycle.qryRecycle do
+  begin
+    Active := False;
+    SQL.Clear;
+
+    SQL.Text := 'SELECT * FROM Login WHERE Username =:Name';
+    Parameters.ParamByName('Name').Value := fInUsername;
+    ExecSQL;
+
+    try
+      with fDetails do
+      begin
+        fUsername := FieldByName('Username').AsString;
+        fLoginID := FieldByName('LoginID').AsString;
+        fPassword := FieldByName('Password').AsString;
+        fSalt := FieldByName('Salt').AsString;
+      end;
+    except
+      raise Exception.Create('User not found');
+    end;
+
+  end;
+end;
 
 end.
